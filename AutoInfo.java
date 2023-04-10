38df8bd39fdf9119a84777014de35a5c1c4b5e47
iralit <li03.1989@gmail.com>
2023-04-10 22:54:42 +0300

    Edit file java

--- AutoInfo.java
+++ AutoInfo.java
@@ -8,40 +8,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class AutoInfo {
     @Test
     public static void getYear (){
         System.setProperty("webdriver.chrome.driver", "C:\\ChrDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("file:///C:/xampp/htdocs/index2.html");
+        driver.get("http://localhost/index2.html");
+
         WebElement title = driver.findElement(By.id("film_name"));
         title.sendKeys("Jaw");
         WebElement bttn = driver.findElement(By.id("get_api_res_btn"));
         bttn.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         WebElement year = driver.findElement(By.xpath("//*[@id=\"api_year\"]"));
         String par = year.getText();
-        System.out.println(par);
-        Assert.assertEquals(year.isDisplayed(), "Year: 2005");
-    }
-    @Test
-    public static void getDirector (){
-        System.setProperty("webdriver.chrome.driver", "C:\\ChrDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("file:///C:/xampp/htdocs/index2.html");
-        WebElement title = driver.findElement(By.id("film_name"));
-        title.sendKeys("Jaw");
-        WebElement bttn = driver.findElement(By.id("get_api_res_btn"));
-        bttn.click();
-        WebElement director = driver.findElement(By.id("api_director"));
-        String par2 = director.getText();
-        System.out.println(par2);
-        Assert.assertEquals(director.isDisplayed(), "Director: Alex Dawson");
+        Assert.assertEquals(par, "Year: 2005");
     }
     public static void main(String[] args) {
-        getDirector();
         getYear();
-
     }
-
 }
